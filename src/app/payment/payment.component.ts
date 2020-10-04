5e03b4da12c8dae0883bcbe96ae1e52b684e8a9f
Ashish Debnath <ashish.debnath@nineleaps.com>
2020-10-04 16:30:44 +0530

    amount changed

--- src/app/payment/payment.component.ts
+++ src/app/payment/payment.component.ts
@@ -1,13 +1,18 @@
-import { ApiService } from './../shared/services/api-service/api.service';
-import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { Title } from '@angular/platform-browser';
+import { ApiService } from "./../shared/services/api-service/api.service";
+import { Component, OnInit } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  FormControl,
+  Validators,
+} from "@angular/forms";
+import { Router } from "@angular/router";
+import { Title } from "@angular/platform-browser";
 
 @Component({
-  selector: 'app-payment',
-  templateUrl: './payment.component.html',
-  styleUrls: ['./payment.component.css']
+  selector: "app-payment",
+  templateUrl: "./payment.component.html",
+  styleUrls: ["./payment.component.css"],
 })
 export class PaymentComponent implements OnInit {
   public PaymentFormGroup: FormGroup;
@@ -15,55 +20,44 @@ export class PaymentComponent implements OnInit {
   constructor(
     private apiService: ApiService,
     private fb: FormBuilder,
-    private route: Router, private titleService: Title
+    private route: Router,
+    private titleService: Title
   ) {
-    this.titleService.setTitle('Payment')
-    this.PaymentFormGroup = this.fb.group(
-      {
-        name: new FormControl('', [Validators.required]),
-        email: new FormControl('', [Validators.required]),
-        amount: new FormControl('', [Validators.required]),
-        phone: new FormControl('', [Validators.required]),
-      },
-    );
+    this.titleService.setTitle("Payment");
+    this.PaymentFormGroup = this.fb.group({
+      name: new FormControl("", [Validators.required]),
+      email: new FormControl("", [Validators.required]),
+      amount: new FormControl("", [Validators.required]),
+      phone: new FormControl("", [Validators.required]),
+    });
   }
 
   userId;
-  name = localStorage.getItem('name')
-  email = localStorage.getItem('email')
-  amount = 500
+  name = localStorage.getItem("name");
+  email = localStorage.getItem("email");
+  amount = 500;
   phone;
   ngOnInit() {
-
     const query = {
-      email: localStorage.getItem('email')
-    }
-    this.apiService.getProfileDetails(query).subscribe(data => {
+      email: localStorage.getItem("email"),
+    };
+    this.apiService.getProfileDetails(query).subscribe((data) => {
       if (data.status == 200) {
         this.userId = data.body._id;
-
       }
-
-    })
-
+    });
   }
   onSubmitPaymentForm(form) {
-    var formData = form.value
+    var formData = form.value;
     formData.returnUrl = `https://ninemiles-project.firebaseapp.com/payment-success/${this.userId}`;
 
-    const orderId = "orderId_" + form.value.phone
+    const orderId = "orderId_" + form.value.phone;
     formData.orderId = orderId;
     this.apiService.makePayment(formData).subscribe((response: any) => {
-      if (response.body.status == 'OK') {
-        var url = response.body.paymentLink
+      if (response.body.status == "OK") {
+        var url = response.body.paymentLink;
         window.location.href = url;
       }
-    })
-
+    });
   }
-
 }
-
-
-
-
